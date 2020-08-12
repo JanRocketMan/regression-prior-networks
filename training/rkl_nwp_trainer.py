6cecfb318deb3def4193c9e24561ce5693c21d81
Sergey Chervontsev <chervontsev@zomb-research-cv.zombie.yandex.net>
2020-08-12 14:30:35 +0300

    Added lots of logging to fix issues in large-scale RKL training

--- training/rkl_nwp_trainer.py
+++ training/rkl_nwp_trainer.py
@@ -34,9 +34,74 @@ class NWPriorRKLTrainer(SingleDistributionTrainer):
         ood_loader=None, save_path=None, load_path=None
     ):
         if ood_loader is None:
+            raise Exception("Training with RKL objective requires OOD data.")
+        self.estimate_targets_avg_mean_var(train_loader)  # Required for Prior
+
+        if load_path is not None and os.path.isfile(load_path + '/traindata.ckpt'):
+            init_epoch, global_step = self.load_current_state(load_path)
+        else:
+            init_epoch, global_step = 0, 0
+            self.logger = self.logger_cls(logdir=self.logger_logdir)
+
+        if not hasattr(self, 'max_steps'):
+            self.max_steps = len(train_loader) * self.epochs
+            self.len_train_loader = len(train_loader)
+
+        print("Starting training for %d steps" % self.max_steps)
+
+        # Memory-efficient cycling of ood dataset with trick by
+        # https://github.com/pytorch/pytorch/issues/1917#issuecomment-433698337
+        ood_iterator = iter(ood_loader)
+
+        for epoch in range(init_epoch, self.epochs):
+            self.loss_stats.reset()
+            self.timing_stats.reset()
+
+            start_time = time()
+            self.model.train()
+            for i, batch in enumerate(train_loader):
+                try:
+                    batch_ood = next(ood_iterator)
+                except StopIteration:
+                    ood_iterator = iter(ood_loader)
+                    batch_ood = next(ood_loader)
+                batch = (batch, batch_ood)
+
+                current_step = global_step + i
+                self.current_step = current_step
+
+                self.optimizer.zero_grad()
+                loss = self.train_step(batch)
+                self.update_step(loss)
+
+                with torch.no_grad():
+                    self.logging_step(
+                        val_loader, current_step,
+                        epoch, i, self.len_train_loader
+                    )
+
+                elapsed_time = time() - start_time
+                self.timing_stats.update(elapsed_time)
+                self.loss_stats.update(loss.item())
+                start_time = time()
+
+            global_step += self.len_train_loader
+
+            self.model.eval()
+            with torch.no_grad():
+                self.eval_step(val_loader, global_step, epoch)
+            if save_path is not None:
+                self.save_current_state(save_path, global_step, epoch)
+
+    def train(
+        self, train_loader, val_loader,
+        ood_loader=None, save_path=None, load_path=None
+    ):
+        if ood_loader is None:
             raise Exception("Training with RKL objectives requires OOD data.")
         self.estimate_targets_avg_mean_var(train_loader)  # Required for Prior
-        mixed_loader = zip(train_loader, cycle(ood_loader))
+        mixed_loader = zip(train_loader, ood_loader)
+        #mixed_loader = zip(train_loader, cycle(ood_loader))
         """
         With only zip() the iterator will be exhausted when the length
         is equal to that of the smallest dataset.
@@ -63,6 +128,24 @@ class NWPriorRKLTrainer(SingleDistributionTrainer):
         ood_loss = self.loss_fn(
             ood_output_distr, ood_prior_distr, None
         )
+        for param_n in ['loc', 'precision_diag', 'belief']:
+            self.logger.add_scalar(
+                'Prior/OOD_' + param_n + '_mean', getattr(ood_prior_distr, param_n).mean().item(),
+                self.current_step
+            )
+            self.logger.add_scalar(
+                'Prior/OOD_' + param_n + '_std', getattr(ood_prior_distr, param_n).std().item(),
+                self.current_step
+            )
+        for param_n in ['loc', 'precision_diag', 'belief']:
+            self.logger.add_scalar(
+                'Output/OOD_' + param_n + '_mean', getattr(ood_output_distr, param_n).mean().item(),
+                self.current_step
+            )
+            self.logger.add_scalar(
+                'Output/OOD_' + param_n + '_std', getattr(ood_output_distr, param_n).std().item(),
+                self.current_step
+            )
 
         # Predict & get loss on in-domain data
         switch_bn_updates(self.model, "train")
@@ -72,19 +155,54 @@ class NWPriorRKLTrainer(SingleDistributionTrainer):
             id_output_distr, id_prior_distr, id_targets
         )
 
+        for param_n in ['loc', 'precision_diag', 'belief']:
+            self.logger.add_scalar(
+                'Prior/ID_' + param_n + '_mean', getattr(id_prior_distr, param_n).mean().item(),
+                self.current_step
+            )
+            self.logger.add_scalar(
+                'Prior/ID_' + param_n + '_std', getattr(id_prior_distr, param_n).std().item(),
+                self.current_step
+            )
+        for param_n in ['loc', 'precision_diag', 'belief']:
+            self.logger.add_scalar(
+                'Output/ID_' + param_n + '_mean', getattr(id_output_distr, param_n).mean().item(),
+                self.current_step
+            )
+            self.logger.add_scalar(
+                'Output/ID_' + param_n + '_std', getattr(id_output_distr, param_n).std().item(),
+                self.current_step
+            )
+
         return id_loss + self.loss_params["ood_coeff"] * ood_loss
 
     def loss_fn(self, predicted_distr, prior_distr, targets=None):
         if targets is not None:
-            return -predicted_distr.expected_log_prob(
+            n_exp_log_prob = -predicted_distr.expected_log_prob(
                 targets.unsqueeze(-1)
-            ).mean() + self.loss_params["inv_real_beta"] * kl_divergence(
+            ).mean()
+            weighted_kl = self.loss_params["inv_real_beta"] * kl_divergence(
                 predicted_distr, prior_distr
             ).mean()
+            self.logger.add_scalar(
+                "Train/ID_neg_exp_log_prob", n_exp_log_prob.item(),
+                self.current_step
+            )
+            self.logger.add_scalar(
+                "Train/ID_weighted_kl", weighted_kl.item(),
+                self.current_step 
+            )
+
+            return n_exp_log_prob + weighted_kl
         else:
-            return self.loss_params["inv_real_beta"] * kl_divergence(
+            weighted_kl = self.loss_params["inv_real_beta"] * kl_divergence(
                 predicted_distr, prior_distr
             ).mean()
+            self.logger.add_scalar(
+                "Train/OOD_weighted_kl", weighted_kl.item(),
+                self.current_step 
+            )
+            return weighted_kl
 
     def get_prior_distribution(self, inputs):
         prior_mean = torch.repeat_interleave(
